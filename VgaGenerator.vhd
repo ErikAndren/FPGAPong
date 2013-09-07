a93ad6db0856d72b803092002ea67586a28f2359
Erik Zachrisson <erik.andren@gmail.com>
2013-09-07 08:01:51 +0200

    Implement in display checker

--- VgaGenerator.vhd
+++ VgaGenerator.vhd
@@ -12,7 +12,7 @@ port (
 	Rst_N    : in bit1;
 	--
 	Red      : in bit1;
-	Green   : in bit1;
+	Green    : in bit1;
 	Blue     : in bit1;
 	--
 	HSyncN   : out bit1;
@@ -24,15 +24,18 @@ port (
 end entity VgaGenerator;
 
 architecture rtl of VgaGenerator is
-	signal HCnt_D, HCnt_N : word(HCntW-1 downto 0);
-	signal VCnt_D, VCnt_N : word(VCntW-1 downto 0);
+	signal HCnt_D, HCnt_N         : word(HCntW-1 downto 0);
+	signal VCnt_D, VCnt_N         : word(VCntW-1 downto 0);
 	signal GenHSync_N, GenVSync_N : bit1;
 	--
 	signal IncHSyncCnt : bit1;
 	signal IncVSyncCnt : bit1;
-	signal HSyncCnt_D : word(VgaHSyncCntW downto 0);
-	signal VSyncCnt_D : word(VgaVSyncCntW downto 0);
-
+	signal HSyncCnt_D  : word(VgaHSyncCntW downto 0);
+	signal VSyncCnt_D  : word(VgaVSyncCntW downto 0);
+	--
+	signal InDisplayWindow_N, InDisplayWindow_D	 : bit1;
+	--
+	signal InVgaVFrontPorch : bit1;
 begin
 	SyncCountersAsync : process (HCnt_D, VCnt_D)
 	begin
@@ -53,20 +56,43 @@ begin
 		end if;
 	end process;
 	
+	InDisplayCalcProc : process (HCnt_D, VCnt_D)
+	begin
+		InDisplayWindow_N <= '1';
+
+		if (HCnt_D < VgaHFrontPorch + VgaHLeftBorder) then
+			InDisplayWindow_N <= '0';
+		end if;
+
+		if (HCnt_D >= VgaHFrontPorch + VgaHLeftBorder + VgaHVideo) then
+			InDisplayWindow_N <= '0';
+		end if;
+
+		if (VCnt_D < VgaVFrontPorch + VgaVTopBorder) then
+			InDisplayWindow_N <= '0';
+		end if;
+
+		if (VCnt_D >= VgaVFrontPorch + VgaVTopBorder + VgaVVideo) then
+			InDisplayWindow_N <= '0';
+		end if;
+	end process;
+	
 	IncHSyncCnt <= '1' when HSyncCnt_D > 0 or GenHSync_N = '1' else '0';
 	IncVSyncCnt <= '1' when VSyncCnt_D > 0 or GenVSync_N = '1' else '0';
 
 	SyncCountersSync : process(Clk, Rst_N)
 	begin
 		if Rst_N = '0' then
-			HCnt_D     <= (others => '0');
-			VCnt_D     <= (others => '0');
-			HSyncCnt_D <= (others => '0');
-			VSyncCnt_D <= (others => '0');
+			HCnt_D            <= (others => '0');
+			VCnt_D            <= (others => '0');
+			HSyncCnt_D        <= (others => '0');
+			VSyncCnt_D        <= (others => '0');
+			InDisplayWindow_D <= '0';
 	   elsif rising_edge(Clk) then
 			HCnt_D <= HCnt_N;
 			VCnt_D <= VCnt_N;
-			
+			InDisplayWindow_D <= InDisplayWindow_N;
+
 			if IncHSyncCnt = '1' then
 				HSyncCnt_D <= HSyncCnt_D + 1;
 			end if;
@@ -80,8 +106,8 @@ begin
 	HSyncOutAssign : HSyncN <= not IncHSyncCnt;
 	VSyncOutAssign : VSyncN <= not IncVSyncCnt;
 	
-	RedFeed   : RedOut   <= Red;
-	GreenFeed : GreenOut <= Green;
-	BlueFeed  : BlueOut  <= Blue;
+	RedFeed   : RedOut   <= Red   when InDisplayWindow_D = '1' else '0';
+	GreenFeed : GreenOut <= Green when InDisplayWindow_D = '1' else '0';
+	BlueFeed  : BlueOut  <= Blue  when IndisplayWindow_D = '1' else '0';
 end architecture;
 	
--- VgaPack.vhd
+++ VgaPack.vhd
@@ -9,27 +9,28 @@ package VgaPack is
 	constant LineFreq   : integer  := 31469;
 	constant FieldFreq  : integer  := 60; -- 59.94;
 	
-	constant HCnt : positive := VgaClkFreq / LineFreq;
-	constant HCntW : positive := bits(HCnt);
+	constant HCnt       : positive := VgaClkFreq / LineFreq;
+	constant HCntW      : positive := bits(HCnt);
 	
-	constant VCnt : positive := VgaClkFreq / FieldFreq;
-	constant VCntW : positive := bits(VCnt);
+	constant VCnt       : positive := VgaClkFreq / FieldFreq;
+	constant VCntW      : positive := bits(VCnt);
 
 	constant VgaHFrontPorch  : positive := 8;
-	constant VgaHSync        : positive := 96;
-	constant VgaHSyncCnt     : positive := VgaHSync;
-	constant VgaHSyncCntW       : positive := bits(VgaHSyncCnt);
-	constant VgaHBackPorch   : positive := 40;
 	constant VgaHLeftBorder  : positive := 8;
 	constant VgaHVideo       : positive := 640;
 	constant VgaHRightBorder : positive := 8;
+	constant VgaHSync        : positive := 96;
+	constant VgaHSyncCnt     : positive := VgaHSync;
+	constant VgaHSyncCntW    : positive := bits(VgaHSyncCnt);
+	constant VgaHBackPorch   : positive := 40;
 	constant VgaHLine        : positive := VgaHFrontPorch + 
 	                                       VgaHSync + 
 														VgaHBackPorch + 
 														VgaHLeftBorder + 
 														VgaHVideo +
 														VgaHRightBorder;
-	
+	constant VgaHLineW        : positive := bits(VgaHLine);
+
 	constant VgaVFrontPorch   : positive := 2;
 	constant VgaVSync         : positive := 2;
 	constant VgaVSyncCnt      : positive := VgaVSync * VgaHLine;
