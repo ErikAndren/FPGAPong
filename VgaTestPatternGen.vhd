73be0e0cec6b36a2f2ec09db1bd2924005ce0c3d
Erik Zachrisson <erik.andren@gmail.com>
2013-09-09 20:42:51 +0200

    Set rainbow pattern

--- VgaTestPatternGen.vhd
+++ VgaTestPatternGen.vhd
@@ -18,20 +18,20 @@ port (
 end entity;
 
 architecture rtl of VgaTestPatternGen is
-	signal Tick_N, Tick_D : bit1;
+	signal Tick_N, Tick_D : word2;
 begin
-	Tick_N <= not Tick_D;
+	Tick_N <= Tick_D + 1;
 
 	process (Clk, Rst_N)
 	begin
 		if Rst_N = '0' then
-			Tick_D <= '0';
+			Tick_D <= (others => '0');
 		elsif rising_edge(Clk) then
 			Tick_D <= Tick_N;
 		end if;
 	end process;
 
-	Red   <= Tick_D;
-	Green <= not Tick_D;
-	Blue  <= '1';
+	Red   <= '1' when Tick_D = "00" else '0';
+	Green <= '1' when Tick_D = "01" else '0';
+	Blue  <= '1' when Tick_D = "10" else '0';
 end architecture;
